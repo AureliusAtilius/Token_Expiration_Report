5893574863fdc3c9b3656683074ad3b633fd74fb
Josh <j.j.h.lindner@gmail.com>
2021-04-12 16:47:02 -0700

    Added file opening and reading. Checking if the token in each row is enabled and if the date is not expired yet.

--- Token_Expiration_Report.py
+++ Token_Expiration_Report.py
@@ -2,21 +2,30 @@ import sys, csv
 from datetime import *
 
 
-# Take file name at the command line
+# Take file name as a command line argument
 fileName = sys.argv[1]
 
-# Get today's date
-today = date.today()
+# Get today's date using datetime.today()
+today = datetime.today()
 
-#TODO: Open file
-        #TODO: Iterate through each row:
-                
-                #TODO: Check whether or not Token is active
-
-                #TODO: Check whether date is after Today's date
-
-                #TODO: Store user name and expiration date
-
+sortedDates={}
+# Open file
+with open(fileName) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        # Iterate through each row:
+        for row in csv_reader:        
+                # Check whether or not Token is active
+                if row["Account Enabled"]== "Yes":
+                        # Check whether date is after Today's date by converting date string using datetime.strptime("2019-10-22", "%Y-%m-%d")
+                        date = datetime.strptime(row["Token Expiration Date"], "%Y-%m-%d")
+                        if today<date:
+                                
+                                # Store user name and expiration date
+                                if date not in sortedDates:
+                                        sortedDates[date] = []
+                                        sortedDates[date].append(row["First Name"]+" "+row["Last Name"])
+                                else:
+                                      sortedDates[date].append(row["First Name"]+" "+row["Last Name"])  
 #TODO: Print user names sorted by expiration dates:
         #<DATE>
                 #<USER>

180e2a98d442c1aef5c2dab145f7f8b38677fb69
Josh <j.j.h.lindner@gmail.com>
2021-04-12 15:19:58 -0700

    Create basic layout of script.

---
+++ Token_Expiration_Report.py
@@ -0,0 +1,27 @@
+import sys, csv
+from datetime import *
+
+
+# Take file name at the command line
+fileName = sys.argv[1]
+
+# Get today's date
+today = date.today()
+
+#TODO: Open file
+        #TODO: Iterate through each row:
+                
+                #TODO: Check whether or not Token is active
+
+                #TODO: Check whether date is after Today's date
+
+                #TODO: Store user name and expiration date
+
+#TODO: Print user names sorted by expiration dates:
+        #<DATE>
+                #<USER>
+                #<USER>
+                #<USER>
+        #<DATE>
+                #<USER>
+                #<USER>
